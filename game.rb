769d285d617ecd9671c555a0912699d93fa53d42
Gustavo <gustavoguimaraes@gmail.com>
2013-12-25 01:56:21 -0500

    Fix date matching

--- game.rb
+++ game.rb
@@ -8,9 +8,9 @@ module Game
 
     before do 
       @count_today = Time.new
-      @count_birthday = {"tara" => Time.new(2014,7,27), "roberta" => Time.new(2014,10,30), "jen" => Time.new(2014,11,13),"rachel"=> Time.new(2014,11,13),"sandy" => Time.new(2014,3,15), "gustavo" => Time.new(2014,9,19),"baby jesus" => Time.new(2013,12,25), "daniel" => Time.new(2014,5,30)}
+      @count_birthday = {"tara" => Time.new(2014,7,26), "roberta" => Time.new(2014,10,30), "jen" => Time.new(2014,11,13),"rachel"=> Time.new(2014,11,13),"sandy" => Time.new(2014,3,15), "gustavo" => Time.new(2014,9,19),"baby jesus" => Time.new(2013,12,25), "daniel" => Time.new(2014,5,30)}
       @today = Date.today.strftime("%m/%d")
-      @birthday = {"tara" =>"07/27", "roberta" => "10/30", "jen" => "11/13","rachel"=> "11/13","sandy" => "3/15", "gustavo" => "9/19", "baby jesus"=> "12/25","daniel" => "5/30"} 
+      @birthday = {"tara" =>"07/26", "roberta" => "10/30", "jen" => "11/13","rachel"=> "11/13","sandy" => "3/15", "gustavo" => "9/19", "baby jesus"=> "12/25","daniel" => "5/30"} 
 
     end
     
@@ -20,13 +20,15 @@ module Game
 
     get'/:person' do
       @person = params[:person]
-      if @today== @birthday 
+      if @today== @birthday[@person] 
         if @person == "baby jesus"
-        @image = "xmas"
-        @result = "Yes, so Merry Christmas!"
+          @image = "xmas"
+          @result = "Yes, so Merry Christmas!"
+          erb :pretend
         else
           @image = "birthday"
           @result = "Yes! Happy Birthday #{@person}"
+          erb :pretend
         end 
       else
         @result = "NO"
@@ -34,8 +36,9 @@ module Game
         @countdown_hours = "#{((@count_birthday[@person].to_i - @count_today.to_i)/60)/60}" 
         @countdown_minutes = "#{((@count_birthday[@person].to_i - @count_today.to_i)/60)}"
         @countdown_seconds = "#{(@count_birthday[@person].to_i - @count_today.to_i)}"
+        erb :index
       end
-      erb :index
+      
     end
 
     get '/:person/pretend' do
